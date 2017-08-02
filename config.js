e29fb6180b042afd08701d404adc94890c5666b9
paul <weByte>
2017-08-02 23:50:45 +0200

    jspm / systemjs / jasmine setup done

---
+++ config.js
@@ -0,0 +1,10 @@
+System.config({
+  baseURL: ".",
+  defaultJSExtensions: false,
+    defaultExtension: false,
+  transpiler: false,
+
+  paths: {
+    "systemjs": "node_modules/systemjs/dist/system.js"
+  }
+});
